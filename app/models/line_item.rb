441f9ce8d71d1f77b690719c5791e26942e878ee
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2025-04-19 13:43:22 +0000

    Task G: Check Out

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
 
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
