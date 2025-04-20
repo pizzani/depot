43ea16047c6d8eb893f9586b104952078f3fa405
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2025-04-20 02:20:30 +0000

    Iteration H2: Active job payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: "Thank you for your order." }
         format.json { render :show, status: :created, location: @order }
       else
@@ -78,6 +78,18 @@ class OrdersController < ApplicationController
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_path, notice: "Your cart is empty"
+      end
+    end
+
+    def pay_type_params
+      if order_params[:pay_type] == "credit_card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "purchase_order"
+        parms.require(:order).permit(:po_number)
+      else
+        {}
       end
     end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+require "pago"
+
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
   enum :pay_type, { check: 0, credit_card: 1, purchase_order: 2 }
@@ -9,6 +11,39 @@ class Order < ApplicationRecord
     cart.line_items.each do |item|
       item.cart_id = nil
       line_items << item
+    end
+  end
+
+  def charge!(pay_type_params)
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type
+    when "check"
+      payment_method = :check
+      payment_details[:routing] = pay_type_params[:routing_number]
+      payment_details[:account] = pay_type_params[:account_number]
+    when "credit_card"
+      payment_method = :credit_card
+      month, year = pay_type_params[:expiration_date].split(//)
+      payment_details[:cc_num] = pay_type_params[:credit_card_number]
+      payment_details[:expiration_month] = month
+      payment_details[:expiration_year] = year
+    when "purchase_order"
+      payment_method = :po
+      payment_details[:po_num] = pay_type_params[:po_number]
+    end
+
+    payment_result = Pago.make_payment(
+      order_id: id,
+      payment_method: payment_method,
+      payment_details: payment_details
+    )
+
+    if payment_result.succeeded?
+      OrderMailer.received(self).deliver_later
+    else
+      raise payment_result.error
     end
   end
 end
