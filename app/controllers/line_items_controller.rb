7b7e1a4c28dfcab40b2e3724a9687396caa2b392
Eduardo Pizzani <83318348+pizzani@users.noreply.github.com>
2025-04-21 12:38:38 +0000

    Iteration I3: Limiting Access

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,5 @@
 class LineItemsController < ApplicationController
+  skip_before_action :authorize, only: %i[ create ]
   include CurrentCart
   include StoreVisits
   before_action :set_cart, only: %i[ create ]
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,5 @@
 class StoreController < ApplicationController
+  skip_before_action :authorize
   include StoreVisits
   include CurrentCart
   before_action :set_cart
